e0ee146b7f3ee376353c143fae25c2fc844ca683
Ramtin Mousavi <40802525+ramtin-mousavi@users.noreply.github.com>
2018-11-09 19:50:06 +0330

    Update Main.cpp

--- Main.cpp
+++ Main.cpp
@@ -2,29 +2,14 @@
 using namespace std;
 
 int fibonacci(int n);
-int power(int a, int b);
-int divide(int a, int b);
+
 
 int main()
 {
 	int inp;
-	printf("HW1--> Insert Positive Integer n To Calculate Fibonacc (n): ");
+	printf("Insert Positive Integer n To Calculate Fibonacc (n): ");
 	cin >> inp;
 	printf("Fibo(%d) = %d\n\n", inp, fibonacci(inp));
-
-	int inp1, inp2;
-	printf("HW2--> Insert 2 Positive Integers To Calculate a^b:\n");
-	cin >> inp1;
-	cin >> inp2;
-
-	printf("%d^%d = %d\n\n", inp1, inp2, power(inp1, inp2));
-
-	int inp3, inp4;
-	printf("HW3--> Insert 2 Positive Integers To Calculate [a/b]:\n");
-	cin >> inp3;
-	cin >> inp4;
-
-	printf("[%d/%d] = %d\n\n", inp3, inp4, divide(inp3, inp4));
 
 	getchar();
 	getchar();
