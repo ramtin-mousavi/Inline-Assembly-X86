68bafb1af17fc0bbf1dc5c22a8faeb331668c42f
Ramtin Mousavi <40802525+ramtin-mousavi@users.noreply.github.com>
2018-11-09 19:51:19 +0330

    Update Fibonacci.cpp

--- Fibonacci.cpp
+++ Fibonacci.cpp
@@ -3,33 +3,34 @@ int fibonacci(int n) {
 	int result;
 
 	__asm {
-		mov ebx, n;
-		cmp ebx, 0;
+		mov ebx, n
+		cmp ebx, 0
 		je CONDITION1
 
-			cmp ebx, 1;
+		cmp ebx, 1
 		je CONDITION1
 
-			dec ebx;
-		mov ecx, ebx;
+		dec ebx;
+		mov ecx, ebx
 
-		mov ebx, 1;
-		mov eax, 1;
+		mov ebx, 1
+		mov eax, 1
 
 	forloop:
-		mov edx, ebx;
-		mov ebx, eax;
-		add eax, edx;
+		mov edx, ebx
+		mov ebx, eax
+		add eax, edx
 		loop forloop
 
-			jmp CONDITION2
+	jmp CONDITION2
 
-			CONDITION1 :
-		mov result, 1;
+	CONDITION1 :
+		mov result, 1
 		jmp CONDITION3
 
-			CONDITION2 :
+	CONDITION2 :
 		mov result, eax;
+		
 	CONDITION3:
 
 	}
